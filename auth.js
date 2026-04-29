cbfd0fed96c408ef345837f9feaa966f52b0e4c0
hansrajsinghdx <hansrajsinghdx@gmail.com>
2026-04-30 00:22:21 +0530

    [H.S] (V-1.1.2) notifications handled with small ui issus

--- auth.js
+++ auth.js
@@ -4,6 +4,7 @@ import bcrypt from 'bcryptjs'
 import { prisma } from '@/lib/prisma'
 import { authConfig } from '@/auth.config'
 
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
   ...authConfig,
   providers: [
